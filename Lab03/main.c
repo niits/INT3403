b60c17cd554209e557602c09f81679558aebed61
Trần Đức Trung <38934471+niits@users.noreply.github.com>
2019-09-21 15:16:26 +0700

    upload lab3 using glDrawArrays

--- Lab03/main.c
+++ Lab03/main.c
@@ -1,8 +1,31 @@
 #include <gl/glut.h>
+#include <windows.h>
+
+float StartShape[18] = {
+    0, 10,
+    -25, 10,
+    -25, 0,
+    -5, 0,
+    -5, -20,
+    5, -20,
+    5, 0,
+    25, 0,
+    25, 10};
+float EndShape[18] = {
+    -25, 10,
+    -25, -20,
+    -5, -20,
+    -5, -10,
+    5, -10,
+    5, -20,
+    25, -20,
+    25, 10,
+    0, 20};
+
 void init();
-void display();
-float t = 0;
-float d = 1;
+void listenMouseClicked(int button, int state, int x, int y);
+void displayWithoutTranform();
+void displayWithTranform();
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
@@ -11,33 +34,49 @@ int main(int argc, char *argv[])
     glutInitWindowPosition(150, 100);
     glutCreateWindow("LAB 3");
     init();
-    glutDisplayFunc(display);
+    glutDisplayFunc(displayWithoutTranform);
+    glutMouseFunc(listenMouseClicked);
     glutMainLoop();
     return 0;
 }
 void init()
 {
     glClearColor(0.0, 0.0, 0.0, 0.0);
+    glColor3f(1.0, 1.0, 0.0);
     glLoadIdentity();
-    glOrtho(-30, 30.0, -30.0, 30.0, -1.0, 1.0);
+    gluOrtho2D(-30, 30, -30, 30);
 }
-void display()
+void listenMouseClicked(int button, int state, int x, int y)
 {
-    float StartShape[9][2] = {{0, 10}, {-25, 10}, {-25, 0}, {-5, 0}, {-5, -20}, {5, -20}, {5, 0}, {25, 0}, {25, 10}};
-    float EndShape[9][2] = {{-25, 10}, {-25, -20}, {-5, -20}, {-5, -10}, {5, -10}, {5, -20}, {25, -20}, {25, 10}, {0, 20}};
-    float m[9][2];
-    for (int i = 0; i < 9; i++)
+    if (state == GLUT_DOWN)
     {
-        m[i][0] = (1 - t) * StartShape[i][0] + t * EndShape[i][0];
-        m[i][1] = (1 - t) * StartShape[i][1] + t * EndShape[i][1];
+        glutDisplayFunc(displayWithTranform);
+        glutPostRedisplay();
     }
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+void displayWithoutTranform()
+{
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(2, GL_FLOAT, 0, StartShape);
     glDrawArrays(GL_LINE_LOOP, 0, 9);
-    glBegin(GL_LINE_LOOP);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glutSwapBuffers();
+}
+void displayWithTranform()
+{
+    static float t = 0;
+    static float d = 1;
+    glClear(GL_COLOR_BUFFER_BIT);
+    float intermediateShape[18];
     for (int i = 0; i < 9; i++)
     {
-        glVertex2d(m[i][0], m[i][1]);
+        intermediateShape[i * 2] = (1 - t) * StartShape[i * 2] + t * EndShape[i * 2];
+        intermediateShape[i * 2 + 1] = (1 - t) * StartShape[i * 2 + 1] + t * EndShape[i * 2 + 1];
     }
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(2, GL_FLOAT, 0, intermediateShape);
+    glDrawArrays(GL_LINE_LOOP, 0, 9);
+    glDisableClientState(GL_VERTEX_ARRAY);
     t += 0.0001 * d;
     if (t >= 1 && d >= 1)
     {
@@ -47,7 +86,6 @@ void display()
     {
         d = 1;
     }
-    glEnd();
     glutSwapBuffers();
     glutPostRedisplay();
 }
